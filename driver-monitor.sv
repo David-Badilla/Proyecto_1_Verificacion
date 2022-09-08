995ff6b4734be9902d1e3ea18821fc5b8e0e105a
David-Badilla <davidbadilla@estudiantec.cr>
2022-09-08 15:43:02 -0600

    cambios

--- driver-monitor.sv
+++ driver-monitor.sv
@@ -14,7 +14,7 @@ class driver #(parameter drvrs = 4 parameter ancho = 16);
 		@(posedge vif.clk);
 		
 		forever begin
-			trans_dut #(.ancho(ancho)) transaccion;
+			trans_dut #(.ancho(ancho)) transaccion; //Crea un objeto para almacenar la transaccion siguiente
 			
 			for(i=0; i < drvrs ; i++)begin  //Reinicio de las variables de todos los buses  *Revisar bien no estoy seguro*
 				vif.D_pop[i]=0;
@@ -26,23 +26,28 @@ class driver #(parameter drvrs = 4 parameter ancho = 16);
 			
 			$display("[%g] El Driver espera una transacción",$time);
 			espera=0;
+
 			@(posedge vif.clk);
 			agnt_drv_mbx.get(transaccion);		//espera a que haya algo en el mailbox bloqueando el avance
 			transaccion.print("Driver: Transaccion recibida");	//imprime lo que recibió 
 			$display("transacciones pendientes en mbx agnt-driver = %g",agnt_drv_mbx.num()); //muestra todas las instrucciones pendientes
 			
+			
+			////////////////////////////////////////////////////////////////////////////////////
+			/// Apartir de aqui ni idea como implemetar todos los fork hijos para cada interface
+			////////////////////////////////////////////////////////////////////////////////////
 			while(espera < transaccion.retardo)begin 	//manejo del retardo
 				@(posedge vif.clk);
 				espera=espera+1;
-				
-				vif.D_pop[transaccion.fuente] = transaccion.dato;   		//Coneccion interfase-transaccion
-			
-			end
+			end // 
 			
 			
 		
 		
-		end
+		
+		
+		
+		end //end del forever
 	
 	
 	
