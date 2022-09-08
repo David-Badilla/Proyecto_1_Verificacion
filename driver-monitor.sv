e72182b6b4247b5a0d1c9a3827a421405b25f951
David-Badilla <davidbadilla@estudiantec.cr>
2022-09-08 14:19:38 -0600

    Modificacion en driver no listo

---
+++ driver-monitor.sv
@@ -0,0 +1,63 @@
+
+
+class driver #(parameter drvrs = 4 parameter ancho = 16);
+	virtual bus_if #(.drvrs(drvrs),.pckg_sz(ancho)) vif;
+	trans_dut_mbx agnt_drv_mbx;
+	trans_dut_mbx drv_chkr_mbx;
+	int espera;
+	int i;
+	
+	task run();
+		$display("[%g] El driver fue inicializado",$time);
+		@(posedge vif.clk);
+		vif.rst=1;
+		@(posedge vif.clk);
+		
+		forever begin
+			trans_dut #(.ancho(ancho)) transaccion;
+			
+			for(i=0; i < drvrs ; i++)begin  //Reinicio de las variables de todos los buses  *Revisar bien no estoy seguro*
+				vif.D_pop[i]=0;
+				vif.pop[i]=0;
+				vif.push[i]=0;
+				vif.rst[i]=0;
+			end
+			
+			
+			$display("[%g] El Driver espera una transacción",$time);
+			espera=0;
+			@(posedge vif.clk);
+			agnt_drv_mbx.get(transaccion);		//espera a que haya algo en el mailbox bloqueando el avance
+			transaccion.print("Driver: Transaccion recibida");	//imprime lo que recibió 
+			$display("transacciones pendientes en mbx agnt-driver = %g",agnt_drv_mbx.num()); //muestra todas las instrucciones pendientes
+			
+			while(espera < transaccion.retardo)begin 	//manejo del retardo
+				@(posedge vif.clk);
+				espera=espera+1;
+				
+				vif.D_pop[transaccion.fuente] = transaccion.dato;   		//Coneccion interfase-transaccion
+			
+			end
+			
+			
+		
+		
+		end
+	
+	
+	
+	endtask
+
+
+
+
+
+
+
+
+
+
+
+
+
+endclass
---
+++ interfase.sv
@@ -0,0 +1,10 @@
+interface bus_if #(parameter drvrs = 4, parameter pckg_sz=32)(input bit clk, input bit rst);
+	//Entradas del bus
+	logic pndng [drvrs-1:0];
+	logic [pckg_sz-1:0] D_pop [drvrs-1:0];
+	
+	//Salidas del bus
+	logic pop [drvrs-1:0];
+	logic push [drvrs-1:0];
+	logic [pckg_sz-1:0] D_push [drvrs-1:0];	
+endinterface
