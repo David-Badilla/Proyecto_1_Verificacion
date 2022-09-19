e27d6d6548d4db8ed05f8f5c803ce2821d6b5b90
David-Badilla <davidbadilla@estudiantec.cr>
2022-09-19 14:47:00 -0600

    Modi gitignore

--- verdiLog/verdi.cmd
+++
@@ -1,5 +0,0 @@
-simSetSimulator "-vcssv" -exec "./salida" -args "+ntb_random_seed=2" -uvmDebug on \
-           -simDelim
-debImport "-i" "-simflow" "-dbdir" "./salida.daidir"
-srcTBInvokeSim
-debExit
